dfc13d1401b73e62bd908c93f741f28ffdf54042
DESKTOP-N8NE5NS\Authuir <authuir@authuir.com>
2017-11-17 13:55:32 +0800

    350

--- algorithm/cpp/350/main.cpp
+++ algorithm/cpp/350/main.cpp
@@ -7,22 +7,16 @@ class Solution {
 public:
 	vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
 		vector<int> rtn;
-		for (auto iter1 = nums1.begin(); iter1 != nums1.end();)
-		{
-			for (auto iter2 = nums2.begin(); iter2 != nums2.end();)
+		for (auto iter1 = nums1.begin(); iter1 != nums1.end(); iter1++)
+			for (auto iter2 = nums2.begin(); iter2 != nums2.end(); iter2++)
 			{
 				if (*iter1 == *iter2)
 				{
 					rtn.push_back(*iter1);
-					iter1 = nums1.erase(iter1, iter1 + 1);
-					iter2 = nums2.erase(iter2, iter2 + 1);
+					iter2 = nums2.erase(iter2);
+					break;
 				}
-				if (iter2 != nums2.end())
-					iter2++;
 			}
-			if (iter1 != nums1.end())
-				iter1++;
-		}
 		return rtn;
 	}
 };
