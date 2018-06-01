45ab88c354bfaf38af1c368edadbce31ad6442ff
DESKTOP-N8NE5NS\Authuir <authuir@authuir.com>
2018-06-01 18:36:25 +0800

    814

---
+++ algorithm/cpp/814.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (root == NULL) return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if (root->left == NULL && root->right == NULL && root->val == 0)
+        {
+            delete(root);
+            return NULL;
+        }
+        return root;
+    }
+};
