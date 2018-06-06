0b43926c38d5c2d8d1bb0e1794a2066a5ec35d51
Authuir <i@authuir.com>
2018-06-06 11:07:52 +0800

    Added Go support for VSC

--- algorithm/golang/771.go
+++ algorithm/golang/771.go
@@ -1,11 +1,17 @@
+package main
+
 func numJewelsInStones(J string, S string) int {
-    rtn := 0
-    for i:=0; i<len(S); i++ {
-        for j:=0; j<len(J); j++ {
-            if S[i] == J[j] {
-                rtn++
-            }
-        }
-    }
-    return rtn;
+	rtn := 0
+	for i := 0; i < len(S); i++ {
+		for j := 0; j < len(J); j++ {
+			if S[i] == J[j] {
+				rtn++
+			}
+		}
+	}
+	return rtn
+}
+
+func main() {
+	numJewelsInStones("abcd", "abcdefg")
 }
