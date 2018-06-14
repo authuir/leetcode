9e44dc3bbd4627283dd39f83150d22de8b4c7c89
Authuir <authuir@authuir.com>
2018-06-14 15:38:15 +0000

    461

---
+++ algorithm/golang/461.go
@@ -0,0 +1,10 @@
+func hammingDistance(x int, y int) int {
+    var rtn int;
+    var i uint32;
+    for i=0; i<32; i++ {
+        if ( ((x>>i)&0x01) != ((y>>i)&0x01) ) {
+            rtn++;
+        }
+    }
+    return rtn;
+}
