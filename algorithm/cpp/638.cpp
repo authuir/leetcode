1b305db7700ccc960a1f84d8b9cf03c13957f37f
Authuir <i@authuir.com>
2018-06-25 15:10:01 +0800

    638.cpp

--- algorithm/cpp/638.cpp
+++ algorithm/cpp/638.cpp
@@ -2,88 +2,117 @@
 #include <vector>
 #include <cmath>
 #include <unordered_map>
+#include <utility>  
+#include <string>
 
 using namespace std;
 
+namespace std
+{
+    template<>
+    struct hash<std::vector<int>>
+    {
+        size_t operator()(std::vector<int> const& v) const
+        {
+			size_t ret = 0;
+			for(auto& i : v) {
+				ret ^= hash<int>()(i);
+			}
+			return ret;
+        }
+    };
+}
+
+
 class Solution {
 public:
-	vector<int> search_needs(vector<int>& needs, vector<int>& special_iter)
-	{
-		vector<int> rtn;
-		for (int i = 0; i<needs.size(); i++)
-			if (needs[i] < special_iter[i])
-			{
-				rtn.push_back(-1);
-				rtn[0] = -1;
-				return rtn;
-			}
-			else
-			{
-				rtn.push_back(needs[i] - special_iter[i]);
-			}
-		return rtn;
-	}
-    int getInput(vector<int>& input)
-    {
-        int rtn = 0;
-        for (int i = 0;i<input.size(); i++)
-        {
-            rtn += input[i]*pow(10,i);
-        }
-        return rtn;
-    }
-	int getDp(vector<int>& price, vector<vector<int>>& special, vector<int>& input) {
-        int xx = getInput(input);
-		auto rst = dp.find(xx);
-		if (rst != dp.end())
-			return rst->second;
-		int rtn = 100000000;
-		int flag = 1;
-		for (auto iter : input)
-		{
+	bool isEmptyInput(vector<int>& input) {
+		for (auto iter : input) {
 			if (iter > 0)
-			{
+				return false;
+		};
+		return true;
+	}
+
+	bool isGetable(vector<int>& offer, vector<int>& require)
+	{
+		for (int i=0; i<require.size(); i++)
+		{
+			if (offer[i] > require[i])
+				return false;
+		}
+		return true;
+	}
+
+	void getDp(vector<int>& price, vector<vector<int>>& offer, pair<vector<int>, int> input) {
+		auto find_result = dp.find(input.first);
+		if (find_result != dp.end()) {
+			if (find_result->second > input.second ) {
+				find_result->second = input.second;
+			} else {
+				return ;
+			}
+		} else {
+			dp.insert(input);
+		}
+
+		if (isEmptyInput(input.first))
+			return;
+
+		for ( auto iter : input.first )
+			cout << iter << '\t';
+		cout << "price: " << input.second << endl;
+		int flag = 1;
+		for (auto iter : offer) {
+			if (isGetable(iter, input.first)) {
+				vector<int> tmp = input.first;
+				for (int i=0; i<tmp.size(); i++) {
+					tmp[i] = tmp[i] - iter[i];
+				}
+				getDp(price, offer, make_pair(tmp, input.second + iter.back()));
 				flag = 0;
-				break;
 			}
 		}
-		if (flag) return 0;
-		for (auto iter : special)
-		{
-			vector<int> tmp = search_needs(input, iter);
-			if (tmp[0] != -1)
-			{
-				int price_here = iter.back() + getDp(price, special, tmp);
-				if (price_here < rtn)
-					rtn = price_here;
-			}
+		if (flag == 1) {
+			int rtn = input.second;
+			for (int i = 0; i<price.size(); i++)
+				rtn += price[i] * input.first[i];
+			vector<int> to_find(price.size(), 0);
+			getDp(price, offer, make_pair(to_find, rtn));
 		}
-		for (int i = 0; i<price.size(); i++)
-		{
-			if (input[i] > 0)
-			{
-				input[i] = input[i] - 1;
-				int price_here = price[i] + getDp(price, special, input);
-				if (price_here < rtn)
-					rtn = price_here;
-				input[i] = input[i] + 1;
+	}
+
+	int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+		for (auto iter = special.begin(); iter != special.end(); ) {
+			int price_sum = 0;
+			for (int i=0; i<price.size(); i++) {
+				price_sum += price[i] * (*iter)[i];
+			}
+			if (price_sum < (*iter).back()) {
+				iter = special.erase(iter);
+			} else {
+				iter ++;
 			}
 		}
 
-		dp.insert({ xx, rtn });
-		return rtn;
+		getDp(price, special, make_pair(needs, 0));
+
+		vector<int> to_find(price.size(), 0);
+		auto rtn = dp.find(to_find);
+		if (rtn != dp.end())
+			return rtn->second;
+		return 0;
 	}
-	int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
-		return getDp(price, special, needs);
-	}
+
 private:
-    unordered_map<int, int> dp;
+    unordered_map<vector<int>, int> dp;
 };
 
 int main()
 {
 	Solution x;
 	vector<int> s1{9,9,7,2,1,3};
+
 	vector<vector<int>> s2;
 	s2.push_back(vector<int>{5,2,6,0,0,2,26});
 	s2.push_back(vector<int>{1,3,2,0,0,6,17});
