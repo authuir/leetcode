557305b083ec36843947a2b2b06169ce0dc9676c
Authuir <i@authuir.com>
2018-06-26 09:51:17 +0800

    50

--- algorithm/cpp/50.cpp
+++ algorithm/cpp/50.cpp
@@ -7,43 +7,24 @@ using namespace std;
 
 class Solution {
 public:
-	double myPow(double x, int n) {
-		map<int, double> M;
-		if (n >= 0)
-		{
-			double rtn = 1.0;
-			bool ox = false;
-			for (unsigned int i = 0,old_i=-1; i<n; )
-			{
-				if (ox)
-					rtn = M[old_i]* M[old_i];
-				else
-					rtn = x*rtn;
-
-				M.insert(map<int, double> ::value_type(i,rtn));
-				if (rtn == 0) break;
-				old_i = i;
-				if (i*i < n && i>2) { i = i*i;ox = true; }
-				else { i++;ox = false; }
-			}
-			return rtn;
-		}
-		else
-		{
-			double rtn = 1.0;
-			for (int i = 0; i<abs(n); i++)
-			{
-				rtn = rtn / x;
-				if (rtn == 0) break;
-			}
-			return rtn;
-		}
-	}
-};
+    double myPow(double x, int n) {
+        double rtn = 1.0;
+        bool is_negative = (n < 0);        
+        unsigned int nn = abs(n);
+        while (nn > 0) {
+            if (nn%2==1) rtn = rtn * x;
+            nn = nn / 2;
+            x = x*x;
+        }
+        if (is_negative)
+            rtn = 1.0 / rtn;
+        return rtn;
+    }
+}; 
 
 int main()
 {
 	Solution s;
-	cout << s.myPow(1, 2147483647);
+	cout << s.myPow(2.0, 10);
 	return 0;
 }
